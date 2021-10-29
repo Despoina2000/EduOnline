00093b97ac6dc899dd8c25c84c9b4beedd82bc81
Ellie <93398997+Hello-Script-Kitty@users.noreply.github.com>
2021-10-29 20:29:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* We can output content to the console, it will help us debug our app */ 
+console.log('I am coding in JavaScript!') 
+
+var number = 10;
+var string = 'Hello there';
+var isPage = true;
+
+document.getElementById('box').innerHTML = STRING; /* the element after '=' gets added to div */
